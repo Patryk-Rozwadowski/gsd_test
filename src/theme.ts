719cecbd4c2f6a94b6cf274cfc9ac30443b75ab9
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-05-15 02:00:50 +0200

    chore: update theme

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,7 @@ import { ITheme } from "./interfaces/theme.interface";
 const theme: ITheme = {
 	colors: {
 		primary: "#fff",
+		border: "#d9d9d9",
 	},
 	styles: {
 		boxShadow: "0 2px 6px 6px rgba(0, 0, 0, 0.1)",
