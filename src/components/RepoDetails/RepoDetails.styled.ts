446a9f5fafc0d65b4a938abb0c24977c050929f0
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-05-16 23:42:10 +0200

    Updates

--- src/components/RepoDetails/RepoDetails.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-import { Flex } from "@chakra-ui/react";
-
-const RepoDetailInfoContainer = styled(Flex)`
-	justify-content: center;
-	flex-direction: column;
-`;
-
-export { RepoDetailInfoContainer };
--- src/utils/sort.ts
+++
@@ -1,3 +0,0 @@
-const sort = (a, b) => a - b;
-
-export { sort };
