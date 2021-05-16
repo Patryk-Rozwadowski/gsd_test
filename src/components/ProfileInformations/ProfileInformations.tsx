201f0ae26355193c98c099c388219540d05e8b22
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-05-16 18:00:50 +0200

    refactor: split ProfileInformation components

--- src/components/ProfileInformations/ProfileInformations.tsx
+++ src/components/ProfileInformations/ProfileInformations.tsx
@@ -17,7 +17,7 @@ import { IRepository } from "../../interfaces/repo.interface";
 import { PopoverContainer, RepoInfoButton, RepoInfoContainer } from "./ProfileInformations.styled";
 import RepoDetails from "../RepoDetails/RepoDetails";
 import { calculateLastDay } from "../../utils/calculateDaysAgo";
-import { sort } from "../../utils/sort";
+import UserRepoList from "../UserRepoList";
 
 const ProfileInformations = ({ login }: Partial<IUserDetailed>): JSX.Element => {
 	const [fetched, setFetched] = useState<boolean>(false);
@@ -39,18 +39,6 @@ const ProfileInformations = ({ login }: Partial<IUserDetailed>): JSX.Element => 
 		return () => ac.abort();
 	}, [login]);
 
-	function calculateIssueRatio() {
-		return repos?.reduce((acc: number, repo: IRepository): number => {
-			return acc + repo.open_issues_count;
-		}, 0);
-	}
-
-	function calculateUserStars(): number {
-		return repos?.reduce((acc: number, repo: IRepository): number => {
-			return acc + repo.stargazers_count;
-		}, 0);
-	}
-
 	return (
 		<>
 			{!fetched ? (
@@ -61,38 +49,11 @@ const ProfileInformations = ({ login }: Partial<IUserDetailed>): JSX.Element => 
 						<TextHeader>{userInformation.name}'s Profile</TextHeader>
 					</PopoverHeader>
 					<PopoverBody>
-						<TextPrimary>Public repos:{userInformation.public_repos}</TextPrimary>
 						<TextPrimary>
 							Joined: {calculateLastDay(userInformation?.created_at)} days ago.
 						</TextPrimary>
-						<TextPrimary>User's Stars: {calculateUserStars()}</TextPrimary>
-						<TextPrimary>Opened issues:{calculateIssueRatio()}</TextPrimary>
 
-						{repos.map((repo: IRepository, index: number) => {
-							return (
-								<Popover key={index} isLazy={true}>
-									<RepoInfoContainer>
-										<TextPrimary>{repo.name}</TextPrimary>
-										<TextPrimary>{repo.stargazers_count}</TextPrimary>
-										<PopoverTrigger>
-											<RepoInfoButton
-												border={0}
-												ml={"15px"}
-												backgroundColor={"#fff"}
-												width={"33%%"}
-												h={"20px"}
-											>
-												Info
-											</RepoInfoButton>
-										</PopoverTrigger>
-									</RepoInfoContainer>
-
-									<PopoverContainer>
-										<RepoDetails login={login} repoName={repo.name} />
-									</PopoverContainer>
-								</Popover>
-							);
-						})}
+						<UserRepoList repos={repos} login={login} />
 					</PopoverBody>
 				</Box>
 			)}
---
+++ src/components/UserRepoList.tsx
@@ -0,0 +1,49 @@
+import {
+	PopoverContainer,
+	RepoInfoButton,
+	RepoInfoContainer,
+} from "./ProfileInformations/ProfileInformations.styled";
+import { TextPrimary } from "./Typography/Typography.styled";
+import { Popover, PopoverTrigger } from "@chakra-ui/react";
+import RepoDetails from "./RepoDetails/RepoDetails";
+import React from "react";
+import { IRepository } from "../interfaces/repo.interface";
+
+interface IUserRepoList {
+	repos: IRepository[];
+	login: string;
+}
+
+const UserRepoList = ({ repos, login }: IUserRepoList) => {
+	return (
+		<>
+			{repos.map((repo: IRepository, index: number) => {
+				return (
+					<Popover key={index} isLazy={true}>
+						<RepoInfoContainer>
+							<TextPrimary>{repo.name}</TextPrimary>
+							<TextPrimary>{repo.stargazers_count}</TextPrimary>
+							<PopoverTrigger>
+								<RepoInfoButton
+									border={0}
+									ml={"15px"}
+									backgroundColor={"#fff"}
+									width={"33%%"}
+									h={"20px"}
+								>
+									Info
+								</RepoInfoButton>
+							</PopoverTrigger>
+						</RepoInfoContainer>
+
+						<PopoverContainer>
+							<RepoDetails login={login} repoName={repo.name} />
+						</PopoverContainer>
+					</Popover>
+				);
+			})}
+		</>
+	);
+};
+
+export default UserRepoList;
