7fd4e4d675f2e3a590e1f013fdd418d4df8bbf24
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-05-16 23:55:21 +0200

    refactor: remove headers from axios params

--- src/API/axiosInstance.ts
+++ src/API/axiosInstance.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const axiosParams = {
 	baseURL: process.env.REACT_APP_GH,
-	headers: {
-		authorization: `token ${process.env.REACT_APP_TOKEN}`,
-	},
 };
 
 const axiosInstance = axios.create(axiosParams);
